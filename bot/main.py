7aab709db4941ee9e491703703ca81b100e4410d
unnavocce <unnavocce@gmail.com>
2021-02-21 16:47:08 +0200

    почти

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,20 @@
-import logging
-from maia import AutoInstaller, printr
-try:
-    import pyautogui
-    import keyboard
-except ImportError:
-    AutoInstaller()
+import pyautogui
 
+button = None
+start = input("ru/eng ?")
+
+if start == "ru":
+    print("ru")
+    button = "RuButton.png"
+elif start == "eng":
+    print("eng")
+    button = "EngButton.png"
+else:
+    print("else")
+
+def main():
+    while True:
+        cords = pyautogui.locateCenterOnScreen(button)
+        pyautogui.click(cords)
 
 
