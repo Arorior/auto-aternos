db2b0b72048beff0b8c7a4172a5ffbc7b813612c
unnavocce <unnavocce@gmail.com>
2021-02-21 15:11:22 +0200

    program base

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,18 @@
+import PySimpleGUI
+start = input("ru/eng ?")
+
+def ru():
+    if input("Введи start для запуска бота") == "start":
+        main()
+
+def eng():
+    if input("Enter start to start the bot") == "start":
+        main()
+
+def main():
+
+if start == "ru":
+   ru()
+elif start == "eng":
+    eng()
 
