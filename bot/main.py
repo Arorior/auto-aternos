144d1bee7691c1db62d86392a4a01adb447a6422
unnavocce <unnavocce@gmail.com>
2021-02-22 18:00:01 +0200

    updated

--- bot/main.py
+++ bot/main.py
@@ -1,22 +1,37 @@
 import pyautogui
 
-lang = open('Languages', 'r')
+language = open('Languages', 'r').readlines()
 button = None
+lang_inside = None
 start = input("ru/eng?\n")
 
-if start == "ru":
-    for line in lang.readlines():
-        print(line)
-    button = "RuButton.png"
-elif start == "eng":
-    print("eng")
-    button = "EngButton.png"
-else:
-    print("else")
-
 def main():
-    while True:
-        cords = pyautogui.locateCenterOnScreen(button)
-        pyautogui.click(cords)
+    if input() == "start":
+        if lang_inside == "ru":
+            print(language[2].strip())
+        elif lang_inside == "eng":
+            print(language[7].strip())
+        while True:
+            cords = pyautogui.locateCenterOnScreen(button)
+            pyautogui.click(cords)
+            if cords != None:
+                if lang_inside == "ru":
+                    print(language[3].strip())
+                elif lang_inside == "eng":
+                    print(language[9].strip())
+                break
+    else:
+        print(language[11].strip())
 
-
+if start == "ru":
+    print(language[1].strip())
+    button = "RuButton.png"
+    lang_inside = "ru"
+    main()
+elif start == "eng":
+    print(language[6].strip())
+    button = "EngButton.png"
+    lang_inside = "eng"
+    main()
+else:
+    print(language[11].strip())
