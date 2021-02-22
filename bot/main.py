7936dffc7d4864018b0a1c1b57ea3ee0bff61b27
Arorior <>
2021-02-22 21:38:17 +0300

    syntax edit

--- bot/main.py
+++ bot/main.py
@@ -3,10 +3,6 @@ import platform
 import plyer
 import pyautogui
 
-language = open('Languages', 'r').readlines()
-button = None
-lang_inside = None
-start = input("ru/eng?\n").lower()
 
 def main():
     if input() == "start":
@@ -17,18 +13,17 @@ def main():
         while True:
             cords = pyautogui.locateCenterOnScreen(button)
             pyautogui.click(cords)
-            if cords != None:
+            if cords is not None:
                 if lang_inside == "ru":
                     print(language[3].strip())
-                    push(title=language[4].strip(),      \
-                    message=language[5].strip())
+                    push(title=language[4].strip(), message=language[5].strip())
                 elif lang_inside == "eng":
                     print(language[10].strip())
-                    push(title = language[11].strip(),   \
-                    message = language[12].strip())
+                    push(title=language[11].strip(), message=language[12].strip())
                 break
     else:
         print(language[15].strip())
+
 
 def push(title, message):
     plt = platform.system()
@@ -42,24 +37,25 @@ def push(title, message):
         notify-send "{title}" "{message}"
         '''
     elif plt == "Windows":
-        plyer.notification.notify\
-            (message= message,
-             app_name= "auto-aternos",
-             title= title)
+        plyer.notification.notify(message=message,
+                                  app_name="auto-aternos",
+                                  title=title)
         return
     else:
         return
     os.system(command)
 
-if start == "ru":
-    print(language[1].strip())
-    button = "RuButton.png"
-    lang_inside = "ru"
+
+if __name__ == '__main__':
+    language = open('Languages', 'r').readlines()
+    button = None
+    lang_inside = None
+    start = input("[ru/en] ").lower()
+    if start not in ('ru', 'en'):
+        exit(0)
+    is_ru = True if start == 'ru' else False
+
+    print(language[1].strip() if is_ru else language[8].strip())
+    button = "RuButton.png" if is_ru else "EngButton.png"
+    lang_inside = start
     main()
-elif start == "eng":
-    print(language[8].strip())
-    button = "EngButton.png"
-    lang_inside = "eng"
-    main()
-else:
-    print(language[15].strip())
